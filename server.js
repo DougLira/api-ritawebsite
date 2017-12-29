cb6f464b53776b392623631f4d882179c1b7c07a
Douglas <dodo_1828@hotmail.co>
2017-12-29 09:44:19 -0200

    publish

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./config/express');
 
-require('./config/database')('localhost/ritaimoveis');
+require('./config/database')('douglas:douglas18@naboo.mongodb.umbler.com:39740/ritaimoveis');
 
 var port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Servidor rodando na porta ' + port));
