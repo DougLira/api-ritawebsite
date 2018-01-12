6f708c725f6047acaf6caeeb2e57849a52f68d2e
DougLira <dodo_1828@hotmail.com>
2018-01-12 13:11:25 -0200

    deploying api

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const app = require('./config/express');
 
-// require('./config/database')('douglas:douglas18@naboo.mongodb.umbler.com:39740/ritaimoveis');
-require('./config/database')('localhost/ritaimoveis');
+if (process.env.NODE_ENV === 'development') {
+
+    require('./config/database')('localhost/ritaimoveis');
+} else {
+
+    require('./config/database')('douglas:douglas18@naboo.mongodb.umbler.com:39740/ritaimoveis');
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Servidor rodando na porta ' + port));
