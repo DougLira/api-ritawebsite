77bdfeabdfa4a0c370694c6019380c64ef93c3f4
DougLira <dodo_1828@hotmail.com>
2018-01-12 13:29:02 -0200

    deploying api

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'development') {
     require('./config/database')('localhost/ritaimoveis');
 } else {
 
-    require('./config/database')('douglas:douglas18@naboo.mongodb.umbler.com:39740/ritaimoveis');
+    require('./config/database')('douglas:douglas18@naboo.mongodb.umbler.com:46253/ritaimoveis');
 }
 
 const port = process.env.PORT || 3000;
