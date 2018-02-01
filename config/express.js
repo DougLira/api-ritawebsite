f2d2d8b382347b4bc4a69d33331f20a7a4ce54a1
Douglas Lira <dodo_1828@hotmail.com>
2018-02-01 20:26:30 -0200

    feat: multiple cors

--- config/express.js
+++ config/express.js
@@ -1,49 +1,58 @@
-const express = require('express'),
-    app = express(),
-    consign = require('consign'),
-    expressaValidator = require('express-validator'),
-    bodyParser = require('body-parser'),
-    cors = require('cors');
+const express = require("express"),
+  app = express(),
+  consign = require("consign"),
+  expressaValidator = require("express-validator"),
+  bodyParser = require("body-parser");
 
-app.set('secret', 'comoumdiadedomingo');
+app.set("secret", "comoumdiadedomingo");
 
-app.options('*', cors());
-
-app.use(bodyParser.raw({type: 'application/octet-stream', limit: '500mb'}));
-app.use(bodyParser.json({type: '*/*', limit: '500mb'}));
-app.use(bodyParser.urlencoded({extended: true, limit: '500mb'}));
+app.use(bodyParser.raw({ type: "application/octet-stream", limit: "500mb" }));
+app.use(bodyParser.json({ type: "*/*", limit: "500mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "500mb" }));
 
 app.use(expressaValidator());
 
 // Add headers
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
+  var allowedOrigins = [
+    // "http://imovelritacorretora.com.br",
+    "http://localhost:4200",
+    "http://localhost:3001"
+  ];
+  var origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  // Website you wish to allow to connect
+  res.setHeader(
+    "ACCESS-CONTROL-ALLOW-ORIGIN",
+    "http://localhost:3001",
+    "https://ritaimoveis.herokuapp.com/"
+  );
 
-    // Website you wish to allow to connect
-    res.setHeader('ACCESS-CONTROL-ALLOW-ORIGIN', 'http://localhost:4200', 'http://localhost:3001', 'https://ritaimoveis.herokuapp.com/');
+  // Request methods you wish to allow
+  res.setHeader("ACCESS-CONTROL-ALLOW-METHODS", "GET, POST, PUT, DELETE");
 
-    // Request methods you wish to allow
-    res.setHeader('ACCESS-CONTROL-ALLOW-METHODS', 'GET, POST, PUT, DELETE');
+  // Request headers you wish to allow
+  res.setHeader("ACCESS-CONTROL-ALLOW-HEADERS", "Content-type, x-access-token");
 
-    // Request headers you wish to allow
-    res.setHeader('ACCESS-CONTROL-ALLOW-HEADERS', 'Content-type, x-access-token');
+  // List of headers that are to be exposed to the XHR front-end object
+  res.setHeader("ACCESS-CONTROL-EXPOSE-HEADERS", "x-access-token");
 
-    // List of headers that are to be exposed to the XHR front-end object
-    res.setHeader('ACCESS-CONTROL-EXPOSE-HEADERS', 'x-access-token');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("ACCESS-CONTROL-ALLOW-CREDENTIALS", true);
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('ACCESS-CONTROL-ALLOW-CREDENTIALS', true);
-
-    // Pass to next layer of middleware
-    next();
+  // Pass to next layer of middleware
+  next();
 });
 
-consign({cwd: 'app'})
-    .include('models')
-    .then('api')
-    .then('routes/site.js')
-    .then('routes/auth.js')
-    .then('routes')
-    .into(app);
+consign({ cwd: "app" })
+  .include("models")
+  .then("api")
+  .then("routes/site.js")
+  .then("routes/auth.js")
+  .then("routes")
+  .into(app);
 
 module.exports = app;
